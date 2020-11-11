3dd631a744d43d3a19d11792eee7d9992ceb5857
Emily Nguyen <eminguyen@chapman.edu>
2020-11-11 12:34:21 -0800

    change code

--- username.sh
+++ username.sh
@@ -1,19 +1,16 @@
 #! /bin/bash
 # username.sh
 # Emily Nguyen 
-echo "Enter a username: "
-echo "The rules are:"
-echo "The only characters that can be used are"
-echo "- lower case letters,"
-echo "- digits, and"
-echo "- the underscore character"
-echo "It must start with a lower case letter"
-echo "It must contain at least three but no more than 12 characters"
-read ZIP
-while echo "$ZIP" | egrep -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+echo "The rules for a valid username are:"
+echo "- The only characters that can be used are lower case letters, digits, and the underscore character"
+echo "- It must start with a lower case letter"
+echo "- It must contain at least three but no more than 12 characters"
+echo "Enter a valid username: "
+read USERNAME 
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a username: "
-	read ZIP
+	read USERNAME
 done
 echo "Thank you"
