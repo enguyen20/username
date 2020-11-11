2c16e0b7b879439ab59a7a89db7d6f2ad85dbf4b
Emily Nguyen <eminguyen@chapman.edu>
2020-11-11 12:46:57 -0800

    Code completed

--- username.sh
+++ username.sh
@@ -4,7 +4,7 @@
 echo "The rules for a valid username are:"
 echo "- The only characters that can be used are lower case letters, digits, and the underscore character"
 echo "- It must start with a lower case letter"
-echo "- It must contain at least three but no more than 12 characters"
+echo "- It must contain at least three, but no more than 12 characters"
 echo "Enter a valid username: "
 read USERNAME 
 while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
